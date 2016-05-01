69270ea3595e1747e8ab8673031a6dbdddf99916
Cameron Taggart <cameron@tachyus.com>
2016-04-30 18:32:52 -0700

    simplified code marcro

--- vscode.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# symlinks workaround for vendor directory
-rm -f ./vendor/github.com/matthieudelaro/nut/persist;
-rm -f ./vendor/github.com/matthieudelaro/nut/nvidia;
-ln -s `pwd`/persist `pwd`/vendor/github.com/matthieudelaro/nut/persist;
-ln -s `pwd`/nvidia `pwd`/vendor/github.com/matthieudelaro/nut/nvidia;
-export GOPATH=`pwd`;
-code -w .
